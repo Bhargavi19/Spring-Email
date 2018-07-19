a92f890e925869e52b9e0ee7f4273cba83939c69
projdevcv <projdevcv@gmail.com>
2018-07-19 12:53:45 +0530

    merging test branch into master branch

--- src/main/java/com/vishnu/project/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vishnu.project.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.vishnu.project.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
